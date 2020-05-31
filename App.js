e3f46d246796f28a72e037208dc1c9e01fd76c62
McBryan <solomonmcbryan@gmail.com>
2020-05-31 15:19:25 +0000

    Added Redux Persist

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Provider } from "react-redux";
+import store from './src/Redux/store'
+import AppContainer from "./src/navigation/navigation";
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <AppContainer/>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
